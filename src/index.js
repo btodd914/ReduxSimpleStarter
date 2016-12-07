d32627dab6d4781992b29c3066a2f2304f1dd788
btodd914 <you@example.com>
2016-12-07 12:25:42 -0600

    added lodash

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchBar';
 import YTSearch from 'youtube-api-search';
@@ -32,10 +33,11 @@ class App extends React.Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
 
             return (
                 <div>
-                    <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                    <SearchBar onSearchTermChange={videoSearch} />
                     <VideoDetail video={this.state.selectedVideo} />
                     <VideoList
                         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
